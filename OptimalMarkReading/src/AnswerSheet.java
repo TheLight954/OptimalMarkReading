9fb5eb194aa917e1fc8511e41d6eb34c04f65256
TheLight954 <szhang835@student.fuhsd.org>
2017-03-09 21:50:16 -0800

    Update AnswerSheet.java

--- OptimalMarkReading/src/AnswerSheet.java
+++ OptimalMarkReading/src/AnswerSheet.java
@@ -3,5 +3,17 @@
  */
 public class AnswerSheet {
 	int id;
+	String alphabet = "ABCDE";
 	ArrayList<int>[] answers;
+	public int convertToString(ArrayList<int> a) {
+		for(int i=0; i<a.size()-1; i++) {
+			System.out.print(a.get(i) + " ");
+		}
+		System.out.print(a.get(a.size()-1));
+	}
+	public void toString() {
+		for(int i=0; i<answers.length; i++) {
+			System.out.println((i+1) + " " + answers.convertToString());
+		}
+	}
 }
